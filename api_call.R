a3e68284f1d630e5a567e18ae0c3712d97371f58
stephbuon <steph.buon@gmail.com>
2022-12-26 07:26:52 -0600

    First code

---
+++ api_call.R
@@ -0,0 +1,45 @@
+library(httr)
+library(jsonlite)
+library(dplyr)
+
+value <- "RwxSOFeVkuTygQpilLiTXWYgtSueUOaf"
+url <- "https://www.ncei.noaa.gov/cdo-web/api/v2/data?datasetid=GHCND&locationid=ZIP:28801&startdate=2010-05-01&enddate=2010-05-01&units=standard"
+a <- GET(url, add_headers("token" = value))
+
+b <- fromJSON(rawToChar(a$content), flatten = TRUE)
+
+c <- b$results
+
+url <- "https://www.ncei.noaa.gov/cdo-web/api/v2/stations/GHCND:USW00013872"
+
+
+
+
+
+
+
+get_weather_data <- function(token, zip, startdate, enddate, units) {
+  base <- "https://www.ncei.noaa.gov/cdo-web/api/v2/data?datasetid=GHCND"
+  zip <- paste0("locationid=ZIP:", zip)
+  startdate <- paste0("startdate=", startdate)
+  enddate <- paste0("enddate=", enddate)
+  units <- paste0("units=", units)
+  api_call <- paste0(base, "&", zip, "&", startdate, "&", enddate, "&", units)
+  print(api_call)
+  response <- GET(api_call, add_headers("token" = token))
+
+  content <- fromJSON(rawToChar(response$content), flatten = TRUE)
+  results <- content$results
+
+  results <- results %>%
+    select(-attributes)
+
+  return(results) }
+
+
+get_weather_data("RwxSOFeVkuTygQpilLiTXWYgtSueUOaf", "28801", "2010-05-01", "2010-05-05", "standard")
+
+
+
+
+
